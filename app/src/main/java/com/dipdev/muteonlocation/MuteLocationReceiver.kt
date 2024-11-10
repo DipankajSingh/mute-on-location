f4c0337da4fdd36998c1efc6a7aa72077963cd54
Dipankaj Singh <dipankajsingh2@.com>
2024-11-10 21:55:31 +0530

    Unmute on muted contact is being developed

--- app/src/main/java/com/dipdev/muteonlocation/MuteLocationReceiver.kt
+++ app/src/main/java/com/dipdev/muteonlocation/MuteLocationReceiver.kt
@@ -3,9 +3,7 @@ package com.dipdev.muteonlocation
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.provider.Telephony
 import android.telephony.TelephonyManager
-import android.util.Log
 
 class MuteLocationReceiver : BroadcastReceiver() {
 
